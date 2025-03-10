ef26d6fde2c469534c3798105be7d3fc08539672
Maulana Yusuf <socionomad@gmail.com>
2025-03-10 22:00:51 +0700

    feat(init): update content terraform to match the success part

--- main.tf
+++ main.tf
@@ -57,27 +57,60 @@ resource "null_resource" "install_dev_tools" {
 resource "null_resource" "setup_zshrc" {
   provisioner "local-exec" {
     command = <<EOT
-      touch ~/.zshrc
-      echo '\n# Pyenv setup' >> ~/.zshrc
+      # Clear existing .zshrc content
+      echo '' > ~/.zshrc
+
+      # Add Oh My Posh conditional setup
+      echo 'if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then' >> ~/.zshrc
+      echo '  eval "$(oh-my-posh init zsh)"' >> ~/.zshrc
+      echo 'fi' >> ~/.zshrc
+      echo '' >> ~/.zshrc
+
+      # Add plugins declaration
+      echo 'plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' >> ~/.zshrc
+      echo '' >> ~/.zshrc
+
+      # Add Oh My Posh theme configuration
+      echo 'eval "$(oh-my-posh init zsh --config ~/.poshthemes/M365Princess.omp.json)"' >> ~/.zshrc
+      echo '' >> ~/.zshrc
+
+      # Add auto-completion setup
+      echo 'autoload -U compinit' >> ~/.zshrc
+      echo 'compinit' >> ~/.zshrc
+      echo 'source ~/.oh-my-posh-completion.zsh' >> ~/.zshrc
+      echo '' >> ~/.zshrc
+
+      # Add Pyenv setup
+      echo '# Pyenv setup' >> ~/.zshrc
       echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.zshrc
       echo 'eval "$(pyenv init --path)"' >> ~/.zshrc
       echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+      echo '' >> ~/.zshrc
 
-      echo '\n# Oh My Posh setup' >> ~/.zshrc
+      # Add Oh My Posh setup again (as per your current .zshrc)
+      echo '# Oh My Posh setup' >> ~/.zshrc
       echo 'if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then' >> ~/.zshrc
       echo '  eval "$(oh-my-posh init zsh)"' >> ~/.zshrc
       echo 'fi' >> ~/.zshrc
       echo 'eval "$(oh-my-posh init zsh --config ~/.poshthemes/M365Princess.omp.json)"' >> ~/.zshrc
+      echo '' >> ~/.zshrc
 
-      echo '\n# Zsh plugins' >> ~/.zshrc
+      # Add Zsh plugins
+      echo '# Zsh plugins' >> ~/.zshrc
       echo 'plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' >> ~/.zshrc
       echo 'source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
       echo 'source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ~/.zshrc
+      echo '' >> ~/.zshrc
 
-      echo '\n# Auto-completion setup' >> ~/.zshrc
+      # Add Auto-completion setup
+      echo '# Auto-completion setup' >> ~/.zshrc
       echo 'autoload -U compinit' >> ~/.zshrc
       echo 'compinit' >> ~/.zshrc
       echo 'source ~/.oh-my-posh-completion.zsh' >> ~/.zshrc
+      echo '' >> ~/.zshrc
+
+      # Add ZSH Highlight Highlighters directory
+      echo 'export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/opt/homebrew/share/zsh-syntax-highlighting/highlighters' >> ~/.zshrc
     EOT
   }
 }
